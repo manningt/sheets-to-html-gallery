7e090b30c045f2465a99f5914865d0b2879e484f
Tom Manning <tom@manningetal.com>
2019-05-17 06:42:18 -0400

    Update build_html.py
    Make credentials file name generic

--- build_html.py
+++ build_html.py
@@ -60,7 +60,7 @@ def main(location='Best Parlor'):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials-accounts-shm.json', SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
